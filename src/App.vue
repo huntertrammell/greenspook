d92b5d28d5334b5f7f65fc4377fdd79e076d6102
Coffee Man <hectorscodingworld@gmail.com>
2021-09-24 23:16:36 -0400

    First push
    First push

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <h1 ref="title">Hello World</h1>
+</template>
+
+<style>
+</style>
+
+<script>
+import { gsap } from "gsap";
+export default {
+  mounted() {
+    const title = this.$refs.title;
+    gsap.from(title, { duration: 1, scale: 1.5, x: 300 });
+  },
+};
+</script>
