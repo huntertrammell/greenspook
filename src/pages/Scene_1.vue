043b1bb508d917c7c02ba4ab07c99955d0ca1b75
Coffee Man <hectorscodingworld@gmail.com>
2022-02-02 06:23:32 -0500

    scene 1 size correctly

--- src/pages/Scene_1.vue
+++ src/pages/Scene_1.vue
@@ -1,5 +1,20 @@
 <template>
   <section class="section-scene-1" role="contentinfo" aria-label="scene 1">
+    <div class="scene-1__story">
+      <img
+        class="scene-1__story--moon"
+        src="/assets/images/scene_1/Creature.png"
+        alt="moon"
+        width="312"
+        height="467"
+      />
+      <div class="scene-1__story--text">
+        <p>Cold winter night - bright stars like teeth</p>
+        <p>Shed light on churchyard underneath</p>
+        <p>Graves like eyes, covered with snow</p>
+        <p>Shine under the moon’s gray glow</p>
+      </div>
+    </div>
     <div class="scene-1__main-focus">
       <img
         class="scene-1__trees scene-1__layers"
@@ -9,13 +24,19 @@
         height="1123"
       />
       <img
-        class="scene-1__trees scene-1__layers"
+        class="scene-1__church scene-1__layers"
         src="/assets/images/scene_1/Church_Grave_Bg.png"
-        alt="Trees and Ground"
+        alt="Church and Ground"
         width="1920"
         height="1123"
       />
-      <div class="scene-1__layers scene-1__graves"></div>
+      <img
+        class="scene-1__graves scene-1__layers"
+        src="/assets/images/scene_1/GRAVES_FOREGROUND.png"
+        alt="Graves"
+        width="1920"
+        height="1123"
+      />
     </div>
   </section>
 </template>
@@ -25,22 +46,39 @@
   background-image: url(/assets/images/scene_1/Sky.png);
 }
 .scene-1 {
+  &__story {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &--moon {
+      padding-top: 5rem;
+      padding-left: 6rem;
+    }
+    &--text {
+      padding-top: 6rem;
+      padding-right: 4rem;
+      font-size: 2.5rem;
+      color: white;
+      font-weight: 600;
+    }
+  }
   &__main-focus {
     position: relative;
     overflow: hidden;
     width: 100%;
-    height: 1920px;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
   }
   &__layers {
     position: absolute;
-    top: 0;
-    width: 100vw;
+    width: 100%;
     height: auto;
+    object-fit: cover;
   }
   &__graves {
-    background-image: url(/assets/images/scene_1/GRAVES_FOREGROUND.png);
-    background-repeat: no-repeat;
-    bottom: 0 !important;
   }
 }
 </style>
